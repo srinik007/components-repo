45749dbe4f502538d7796c5ff53c7aa960b3ea8a
srinik007 <dskanuri@gmail.com>
2022-11-02 16:28:15 +0530

    nav To Record

---
+++ force-app/main/default/lwc/navToRecordDetailPage/navToRecordDetailPage.js
@@ -0,0 +1,18 @@
+import { api, LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class NavToRecordDetailPage extends NavigationMixin(LightningElement) {
+    @api recordId;
+    @api objectApiName;
+    
+    navToRecordView() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: this.objectApiName,
+                actionName: 'view'
+            }
+        });
+    }
+}
